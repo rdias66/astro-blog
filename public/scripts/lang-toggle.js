731935aa49ea45e8f6f7cf7e0e0bd991c890a96b
rdias66 <dias.a@live.com>
2024-10-14 14:01:01 -0300

    page layout with header done + icon and theme dynamic handling done

--- public/scripts/lang-toggle.js
+++ public/scripts/lang-toggle.js
@@ -1,16 +1,11 @@
-const defaultLanguage = 'en' // Default language: "en" for English, "pt" for Portuguese
-
-// Get language data from local storage
+const defaultLanguage = 'en'
 const currentLanguage = localStorage.getItem('language')
 
 function getPreferredLanguage() {
-  // Return language value in local storage if it is set
   if (currentLanguage) return currentLanguage
 
-  // Use the server-side language set via the URL path as the fallback
   const serverLanguage = document.documentElement.getAttribute('lang')
 
-  // Return the server-side language or default language
   return serverLanguage || defaultLanguage
 }
 
@@ -22,35 +17,28 @@ function setLanguagePreference() {
 }
 
 function reflectLanguagePreference() {
-  // Update the "lang" attribute in the <html> tag
   document.documentElement.setAttribute('lang', languageValue)
 
-  // Optionally: Update UI elements to reflect the current language (e.g., toggle button text)
   document
     .querySelector('#language-btn')
     ?.setAttribute('aria-label', languageValue)
 }
 
-// Set language early so no page flashes or issues with initial language loading
 reflectLanguagePreference()
 
 window.onload = () => {
   function setLanguageFeature() {
-    // Reflect current language on load
     reflectLanguagePreference()
 
-    // Find and listen for clicks on the language toggle button
     document.querySelector('#language-btn')?.addEventListener('click', () => {
       languageValue = languageValue === 'en' ? 'pt' : 'en'
       setLanguagePreference()
 
-      // Optionally: Reload the page to fetch new content in the selected language
-      window.location.reload() // This reloads the page to apply the new language
+      window.location.reload()
     })
   }
 
   setLanguageFeature()
 
-  // Runs on view transitions (Astro specific)
   document.addEventListener('astro:after-swap', setLanguageFeature)
 }
--- public/scripts/theme-toggle.js
+++ public/scripts/theme-toggle.js
@@ -2,17 +2,13 @@
 
 const primaryColorScheme = '' // "light" | "dark"
 
-// Get theme data from local storage
 const currentTheme = localStorage.getItem('theme')
 
 function getPreferTheme() {
-  // Return theme value in local storage if it is set
   if (currentTheme) return currentTheme
 
-  // Return primary color scheme if it is set
   if (primaryColorScheme) return primaryColorScheme
 
-  // Return user device's preferred color scheme
   return window.matchMedia('(prefers-color-scheme: dark)').matches
     ? 'dark'
     : 'light'
@@ -35,46 +31,38 @@ function reflectPreference() {
       themeValue === 'light' ? 'fa-solid fa-moon' : 'fa-solid fa-sun'
   }
 
-  // Get a reference to the body element
   const body = document.body
 
-  // Check if the body element exists before using getComputedStyle
   if (body) {
-    // Get the computed styles for the body element
     const computedStyles = window.getComputedStyle(body)
     const bgColor = computedStyles.backgroundColor
 
-    // Set the background color in <meta theme-color ... />
     document
       .querySelector("meta[name='theme-color']")
       ?.setAttribute('content', bgColor)
   }
 }
 
-// Set early so no page flashes / CSS is made aware
 reflectPreference()
 
 window.onload = () => {
   function setThemeFeature() {
-    reflectPreference() // Set on load so screen readers can get the latest value on the button
+    reflectPreference()
 
-    // Listen for clicks on the control
     document.querySelector('#theme-btn')?.addEventListener('click', () => {
-      themeValue = themeValue === 'light' ? 'dark' : 'light' // Toggle the theme
+      themeValue = themeValue === 'light' ? 'dark' : 'light'
       setPreference()
     })
   }
 
   setThemeFeature()
 
-  // Runs on view transitions navigation
   document.addEventListener('astro:after-swap', setThemeFeature)
 }
 
-// Sync with system changes
 window
   .matchMedia('(prefers-color-scheme: dark)')
   .addEventListener('change', ({ matches: isDark }) => {
-    themeValue = isDark ? 'dark' : 'light' // Update theme based on system preference
+    themeValue = isDark ? 'dark' : 'light'
     setPreference()
   })
