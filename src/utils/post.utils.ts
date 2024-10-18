f7bcd970b916bfe54f3299c620e730f5be61d96d
rdias66 <dias.a@live.com>
2024-10-17 22:38:19 -0300

    post utils + featured and recent on home

---
+++ src/utils/post.utils.ts
@@ -0,0 +1,22 @@
+import { SITE } from '@config'
+import type { CollectionEntry } from 'astro:content'
+
+export const getSortedPosts = (posts: CollectionEntry<'blog'>[]) => {
+  return posts
+    .filter(postFilter)
+    .sort(
+      (a, b) =>
+        Math.floor(
+          new Date(b.data.modDatetime ?? b.data.pubDatetime).getTime() / 1000,
+        ) -
+        Math.floor(
+          new Date(a.data.modDatetime ?? a.data.pubDatetime).getTime() / 1000,
+        ),
+    )
+}
+
+const postFilter = ({ data }: CollectionEntry<'blog'>) => {
+  const isPublishTimePassed =
+    Date.now() > new Date(data.pubDatetime).getTime() - SITE.scheduledPostMargin
+  return !data.draft && (import.meta.env.DEV || isPublishTimePassed)
+}
