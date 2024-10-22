17890313b7a3eed46fa379cd1b0b88bdd6b49777
rdias66 <dias.a@live.com>
2024-10-22 19:24:04 -0300

    port fix on astro config

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-import type { Site, SocialObjects } from './types'
+import type { Site } from './types'
 
 export const SITE: Site = {
   website: 'https://blog.rdias66.codes/',
@@ -29,36 +29,3 @@ export const LOGO_IMAGE = {
   width: 216,
   height: 46,
 }
-
-export const SOCIALS: SocialObjects = [
-  {
-    name: 'Github',
-    href: 'https://github.com/rdias66',
-    linkTitle: ` ${SITE.title} on Github`,
-    active: true,
-  },
-  {
-    name: 'Instagram',
-    href: 'https://github.com/satnaing/astro-paper',
-    linkTitle: `${SITE.title} on Instagram`,
-    active: true,
-  },
-  {
-    name: 'LinkedIn',
-    href: 'https://github.com/satnaing/astro-paper',
-    linkTitle: `${SITE.title} on LinkedIn`,
-    active: true,
-  },
-  {
-    name: 'Mail',
-    href: 'mailto:yourmail@gmail.com',
-    linkTitle: `Send an email to ${SITE.title}`,
-    active: false,
-  },
-  {
-    name: 'Twitter',
-    href: 'https://github.com/satnaing/astro-paper',
-    linkTitle: `${SITE.title} on Twitter`,
-    active: false,
-  },
-]
