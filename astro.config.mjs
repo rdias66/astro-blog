e69b16272906d4b929746fdaa9936117d601efba
rdias66 <dias.a@live.com>
2024-10-22 22:47:49 -0300

    package start script updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://blog.rdias66.codes',
   server: { port: 4322 },
   experimental: {
-    contentLayer: true, // Enable Content Layer API
+    contentLayer: true,
   },
 })
